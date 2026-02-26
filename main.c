b68729843ce8b857dd0a6860467b69ecc0051c85
Jamie7846 <96859115+Jamie7846@users.noreply.github.com>
2026-02-26 11:54:20 +1000

    Initial Commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 26-Feb-2026 jawwh
+ * Group-Project
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
