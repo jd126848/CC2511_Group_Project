4e8448e512c4370fa6eeb2fe5dafaeefaba4bd10
Jamie7846 <96859115+Jamie7846@users.noreply.github.com>
2026-03-31 15:54:08 +1000

    Can control CNC including Spindle and microstepping
    Microstepping is untested

--- CNCControl/main.c
+++ CNCControl/main.c
@@ -11,6 +11,17 @@
 #include "mmhal.h"
 
 #define COMMAND_BUFFER_SIZE 64
+#define NUMSPEEDS 5
+#define CNCVERSION 1
+
+const int spindle_pwm_levels[NUMSPEEDS] = {0, 64, 128, 192, 256};
+int spindle_speed = 0;
+
+
+
+int Xcoord;
+int Ycoord;
+int Zcoord;
 
 bool manual_mode = true; // Start in manual mode for testing
 
@@ -49,7 +60,6 @@ void process_input() {
   }
 }
 
-
 int main(void) {
   // Initialise components and variables
   stdio_init_all();
@@ -58,10 +68,72 @@ int main(void) {
   while (true) {
     //  Repeated code here
     if (manual_mode) {
-      process_input();
+      int ch = getchar_timeout_us(0);
+      
+      if (ch != PICO_ERROR_TIMEOUT) {
+        printf("%c\r\n", ch);
+        switch (ch)
+        {
+        case 'a':
+          // for (int i = 0; i < 100; i++)
+          // {
+          //   printf("%d", i);
+          //   gpio_put(XSTEP_PIN, 1);
+          //   sleep_us(mmhal_high_delay_us);
+          //   gpio_put(XSTEP_PIN, 0);
+          //   sleep_us(mmhal_low_delay_us);
+          // }
+          
+          
+          mmhal_step_motors(-1,0,0);
+          break;
+        case 'd':
+          for (size_t i = 0; i < 50; i++)
+          {
+            /* code */
+            mmhal_step_motors(-1,0,0);
+          }
+          
+          mmhal_step_motors(1,0,0);
+          break;
+        case 'w':
+          mmhal_step_motors(0,1,0);
+          break;
+        case 's':
+          mmhal_step_motors(0,-1,0);
+          break;
+        case 'q':
+          mmhal_step_motors(0,0,1);
+          break;
+        case 'e':
+          mmhal_step_motors(0,0,-1);
+          break;
+        case '=':
+        case '+':
+          if (spindle_speed < NUMSPEEDS -1) {
+            spindle_speed++;
+            mmhal_set_spindle_pwm(spindle_pwm_levels[spindle_speed]);
+          }
+          printf("Speed: %d", spindle_speed);
+          break;
+        case '-':
+        case '_':
+          if (spindle_speed > 0) {
+            spindle_speed--;
+            mmhal_set_spindle_pwm(spindle_pwm_levels[spindle_speed]);
+          }
+          printf("Speed: %d", spindle_speed);
+          break;
+        default:
+          // printf("%d", ch);
+          break;
+        }
+      }
+
       // handle_manual_mode();
     }
     else {
+      process_input();
       // handle_command_mode();
     }
   }
--- CNCControl/mmhal.c
+++ CNCControl/mmhal.c
@@ -20,7 +20,7 @@ const int stepper_multipliers[] = {1, -1, 1};
 #endif
 
 volatile int mmhal_high_delay_us = 2000; // Microseconds for step pulse high time
-volatile int mmhal_low_delay_us = 100;   // Microseconds for step pulse low time
+volatile int mmhal_low_delay_us = 1000;   // Microseconds for step pulse low time
 
 /**
  * @brief Initialize GPIO pins and PWM for spindle control
@@ -31,22 +31,31 @@ void mmhal_init()
   gpio_init(X_MODE0_PIN);
   gpio_init(X_MODE1_PIN);
   gpio_init(X_MODE2_PIN);
+  gpio_init(XFLT_PIN);
   gpio_init(Y_MODE0_PIN);
   gpio_init(Y_MODE1_PIN);
   gpio_init(Y_MODE2_PIN);
+  gpio_init(YFLT_PIN);
   gpio_init(SPINDLE_PIN);
+  gpio_init(ENABLE_PIN);
 
   gpio_set_dir(X_MODE0_PIN, GPIO_OUT);
   gpio_set_dir(X_MODE1_PIN, GPIO_OUT);
   gpio_set_dir(X_MODE2_PIN, GPIO_OUT);
+  gpio_set_dir(XFLT_PIN, GPIO_IN);
   gpio_set_dir(Y_MODE0_PIN, GPIO_OUT);
   gpio_set_dir(Y_MODE1_PIN, GPIO_OUT);
   gpio_set_dir(Y_MODE2_PIN, GPIO_OUT);
+  gpio_set_dir(YFLT_PIN, GPIO_IN);
   gpio_set_dir(SPINDLE_PIN, GPIO_OUT);
+  gpio_set_dir(ENABLE_PIN, GPIO_OUT);
   
+  gpio_put(ENABLE_PIN, 0);
   for (int i = 0; i < DIMCOUNT; i++)
   {
     gpio_init(step_pins[i]);
+    gpio_set_dir(step_pins[i], GPIO_OUT);
+    gpio_init(dir_pins[i]);
     gpio_set_dir(dir_pins[i], GPIO_OUT);
     // TODO - Initialise the pins for X, Y & Z steppers, using the
     // step_pins and dir_pins arrays
@@ -63,10 +72,33 @@ void mmhal_init()
 void mmhal_set_spindle_pwm(uint16_t pwm_level)
 {
   // TODO - Implement spindle PWM setting
+  pwm_set_gpio_level(SPINDLE_PIN, pwm_level);
 }
 
 void mmhal_set_microstepping(int x_or_y, mmhal_microstep_mode_t mode)
 {
+  int mode0_pin;
+  const mode_values[6][3] = {
+    {0,0,0},
+    {1,0,0},
+    {0,1,0},
+    {1,1,0},
+    {0,0,1},
+    {1,0,1}
+};
+
+  if (x_or_y) {
+    mode0_pin = 7;
+  }
+  else {
+    mode0_pin = 2;
+  }
+  
+  gpio_put(mode0_pin, mode_values[mode][0]);
+  gpio_put(mode0_pin, mode_values[mode][1]);
+  gpio_put(mode0_pin, mode_values[mode][2]);
+  printf("Microstepping Mode: %d", mode);
+
   // TODO - Implement microstepping mode setting
 }
 
@@ -77,6 +109,55 @@ void mmhal_set_microstepping(int x_or_y, mmhal_microstep_mode_t mode)
  */
 void mmhal_step_motors_impl(int dirs[])
 {
+  printf("%d x, %d y", dirs[XDIM], dirs[YDIM]);
+  dirs[XDIM] *= stepper_multipliers[XDIM];
+  dirs[YDIM] *= stepper_multipliers[YDIM];
+  dirs[ZDIM] *= stepper_multipliers[ZDIM];
+
+  if (dirs[XDIM] == 1) {
+    gpio_put(XDIR_PIN, 1);
+    gpio_put(XSTEP_PIN, 1);
+    sleep_us(mmhal_high_delay_us);
+    gpio_put(XSTEP_PIN, 0);
+    sleep_us(mmhal_low_delay_us);
+  }
+  if (dirs[XDIM] == -1) {
+    gpio_put(XDIR_PIN, 0);
+    gpio_put(XSTEP_PIN, 1);
+    sleep_us(mmhal_high_delay_us);
+    gpio_put(XSTEP_PIN, 0);
+    sleep_us(mmhal_low_delay_us);
+  }
+  if (dirs[YDIM] == 1) {
+    gpio_put(YDIR_PIN, 1);
+    gpio_put(YSTEP_PIN, 1);
+    sleep_us(mmhal_high_delay_us);
+    gpio_put(YSTEP_PIN, 0);
+    sleep_us(mmhal_low_delay_us);
+  }
+  if (dirs[YDIM] == -1) {
+    gpio_put(YDIR_PIN, 0);
+    gpio_put(YSTEP_PIN, 1);
+    sleep_us(mmhal_high_delay_us);
+    gpio_put(YSTEP_PIN, 0);
+    sleep_us(mmhal_low_delay_us);
+  }
+  if (dirs[ZDIM] == 1) {
+    gpio_put(ZDIR_PIN, 1);
+    gpio_put(ZSTEP_PIN, 1);
+    sleep_us(mmhal_high_delay_us);
+    gpio_put(ZSTEP_PIN, 0);
+    sleep_us(mmhal_low_delay_us);
+  }
+  if (dirs[ZDIM] == -1) {
+    gpio_put(ZDIR_PIN, 0);
+    gpio_put(ZSTEP_PIN, 1);
+    sleep_us(mmhal_high_delay_us);
+    gpio_put(ZSTEP_PIN, 0);
+    sleep_us(mmhal_low_delay_us);
+  }
+  
+  
   // TODO - Implement motor stepping logic, using the dirs array
   // to determine which motors to step and in which direction
   // Remember to use the stepper_multipliers array to handle
